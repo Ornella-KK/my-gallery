95cbc8c432307bef69adcf1057bce7766b83865e
Ornella-KK <ornellakankazi@gmail.com>
2020-12-19 11:08:34 +0200

    deleted the location model

--- gallery/models.py
+++ gallery/models.py
@@ -3,5 +3,3 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length =30)
 
-class Location(models.Model):
-    name = models.CharField(max_length =30)
