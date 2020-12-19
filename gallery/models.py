c3d691d29133d97488ed02730fc18ef333ca9565
Ornella-KK <ornellakankazi@gmail.com>
2020-12-19 13:38:42 +0200

    added search model

--- gallery/models.py
+++ gallery/models.py
@@ -27,3 +27,8 @@ class Image(models.Model):
     category = models.ForeignKey(Category)
     address = models.ForeignKey(Location)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def search_by_category(cls,search_images):
+        gallery = cls.objects.filter(title__icontains=search_images)
+        return gallery
