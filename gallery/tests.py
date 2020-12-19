a16d95a03bea81a02bd5048759e51919ba912dfc
Ornella-KK <ornellakankazi@gmail.com>
2020-12-19 12:59:39 +0200

    added tests for image

--- gallery/tests.py
+++ gallery/tests.py
@@ -33,3 +33,23 @@ class LocationTestClass(TestCase):
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
 
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.happiness= Category(first_name = 'Happiness')
+        self.happiness.save_category()
+
+        # Creating a new tag and saving it
+        self.new_location = Location(place = 'kigali')
+        self.new_location.save()
+
+        self.new_image= Image(name = 'Living the best life',description = 'This is a random test Post',category = self.happiness)
+        self.new_image.save()
+
+        self.new_image.address.add(self.new_location)
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
