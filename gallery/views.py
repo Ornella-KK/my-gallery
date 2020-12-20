5c9de1f8b7f8210a3eeba39a809f0d4e8d4a2cce
Ornella-KK <ornellakankazi@gmail.com>
2020-12-20 10:42:06 +0200

    added landing page view

--- gallery/views.py
+++ gallery/views.py
@@ -4,6 +4,22 @@ import datetime as dt
 from django.http  import HttpResponse,Http404
 from .models import *
 
+def home(request):
+    gallery = Image.objects.all()
+    location = Location.objects.all()
+    category = Categories.objects.all()
+
+    if 'location' in request.GET and request.GET['location']:
+        place = request.GET.get('location')
+        gallery = Image.view_location(name)
+
+    elif 'category' in request.GET and request.GET['category']:
+        caty = request.GET.get('categories')
+        gallery = Image.view_category(caty)
+        return render(request, 'all-images.html', {"name":name,"images":images,"cat":cat })
+
+    return render(request,"all-images.html",{"images":images,"location":location,"category":category})
+
 def search_results(request):
 
     if 'category' in request.GET and request.GET["category"]:
