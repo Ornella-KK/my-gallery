6765b36d77d97c6ea2a1c57fab663a8dbb1add35
Ornella-KK <ornellakankazi@gmail.com>
2020-12-20 11:11:16 +0200

    added home and search url

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-#..............
+    url(r'^$', views.home,name='home'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.get_image_by_id,name ='image')
+
 ]
 
 if settings.DEBUG:
