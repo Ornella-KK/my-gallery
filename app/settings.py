47ab3149bf5cfb8796e82ed3aa69f7a0163938a1
Ornella-KK <ornellakankazi@gmail.com>
2021-01-08 09:23:14 +0200

    deployment to heroku

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '342s(s(!hsjd998sde8$=o4$3m!(o+kce2^97kp6#ujhi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ''
 
 # Application definition
 
